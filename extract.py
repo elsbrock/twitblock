86aaa92396bcab287ed24bdf5789e372e265fc78
Simon Elsbrock <simon@iodev.org>
2017-09-10 00:13:46 +0200

    pylint

--- extract.py
+++ extract.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
 import re
-import PyPDF2
 import argparse
 
-parser = argparse.ArgumentParser(description='Extracts usernames from the Twitter advertister list.')
-parser.add_argument('inputfile', type=str, help='Advertister List PDF', default='twitter_advertiser_list.pdf')
-args = parser.parse_args()
-print(args.inputfile)
+import PyPDF2
 
-with open(args.inputfile, 'rb') as pdfFileObj:
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    numberOfPages = pdfReader.getNumPages()
+def run():
+    parser = argparse.ArgumentParser(description='Extracts usernames from the Twitter '
+                                     + 'advertister list.')
+    parser.add_argument('inputfile', type=str, help='Advertister List PDF',
+                        default='twitter_advertiser_list.pdf')
+    args = parser.parse_args()
 
-    for pages in range(numberOfPages):
-        userPage = pdfReader.getPage(pages).extractText();
-        for username in re.findall(r'(@[^@]+)', userPage):
-            print(username)
+    with open(args.inputfile, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        num_pages = pdf_reader.getNumPages()
+
+        for pages in range(num_pages):
+            user_page = pdf_reader.getPage(pages).extractText()
+            for username in re.findall(r'(@[^@]+)', user_page):
+                print(username)
+
+if __name__ == '__main__':
+    run()
--- resolve.py
+++ resolve.py
@@ -1,48 +1,55 @@
 #!/usr/bin/env python
 
 import sys
-import time
-import twitter
 import argparse
 import configparser
 
-parser = argparse.ArgumentParser(description='Converts Twitter Usernames to UIDs.')
-parser.add_argument('-c', dest='configfile', type=str, help='Config file path', default='config.ini')
-parser.add_argument('inputfile', type=str, nargs='?', help='Username input file')
-args = parser.parse_args()
+import twitter
 
-config = configparser.ConfigParser()
-config.read(args.configfile)
+def run():
+    parser = argparse.ArgumentParser(description='Converts Twitter Usernames to UIDs.')
+    parser.add_argument('-c', dest='configfile', type=str, help='Config file path',
+                        default='config.ini')
+    parser.add_argument('inputfile', type=str, nargs='?', help='Username input file')
+    args = parser.parse_args()
 
-oauth = {
-    'ACCESS_TOKEN_KEY': config['twitter.com']['ACCESS_TOKEN_KEY'],
-    'ACCESS_TOKEN_SECRET': config['twitter.com']['ACCESS_TOKEN_SECRET'],
-    'CONSUMER_KEY': config['twitter.com']['CONSUMER_KEY'],
-    'CONSUMER_SECRET': config['twitter.com']['CONSUMER_SECRET'],
-}
+    config = configparser.ConfigParser()
+    config.read(args.configfile)
 
-api = twitter.Api(consumer_key=oauth['CONSUMER_KEY'], consumer_secret=oauth['CONSUMER_SECRET'],
-                  access_token_key=oauth['ACCESS_TOKEN_KEY'], access_token_secret=oauth['ACCESS_TOKEN_SECRET'],
-                  sleep_on_rate_limit=True)
+    oauth = {
+        'ACCESS_TOKEN_KEY': config['twitter.com']['ACCESS_TOKEN_KEY'],
+        'ACCESS_TOKEN_SECRET': config['twitter.com']['ACCESS_TOKEN_SECRET'],
+        'CONSUMER_KEY': config['twitter.com']['CONSUMER_KEY'],
+        'CONSUMER_SECRET': config['twitter.com']['CONSUMER_SECRET']
+    }
 
-if (args.inputfile):
-    users = open(args.inputfile)
-else:
-    users = sys.stdin
+    api = twitter.Api(consumer_key=oauth['CONSUMER_KEY'],
+                      consumer_secret=oauth['CONSUMER_SECRET'],
+                      access_token_key=oauth['ACCESS_TOKEN_KEY'],
+                      access_token_secret=oauth['ACCESS_TOKEN_SECRET'],
+                      sleep_on_rate_limit=True)
 
-for user in users:
-    while True:
-        user = user.strip()
-        try:
-            blocked = api.CreateBlock(screen_name=user, include_entities=False, skip_status=True)
-            print('%s->%s BLOCKED' % (user, blocked.id))
-            break
-        except twitter.error.TwitterError as e:
-            print("error: twitter: %s" % (e), file=sys.stderr)
-            break
-        except OSError:
-            # probably network down
-            print("error: general: %s" % (e), file=sys.stderr)
-            pass
+    if args.inputfile:
+        users = open(args.inputfile)
+    else:
+        users = sys.stdin
 
-users.close()
+    for user in users:
+        while True:
+            user = user.strip()
+            try:
+                blocked = api.CreateBlock(screen_name=user, include_entities=False,
+                                          skip_status=True)
+                print('%s->%s BLOCKED' % (user, blocked.id))
+                break
+            except twitter.error.TwitterError as err:
+                print("error: twitter: %s" % (err), file=sys.stderr)
+                break
+            except OSError as err:
+                # probably network down
+                print("error: general: %s" % (err), file=sys.stderr)
+
+    users.close()
+
+if __name__ == '__main__':
+    run()
